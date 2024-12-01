776601f6d4e1c252aa6243fce8ef5ebb8a85d263
yvonne <105149333+ncnuyvonne@users.noreply.github.com>
2024-12-01 16:27:43 +0800

    Update non-blocking2.c

--- non-blocking2.c
+++ non-blocking2.c
@@ -16,10 +16,13 @@ int main(int argc, char ** argv)
     if (rank == 0) {
 	x=10;
 	MPI_Isend(&x, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, &req1);
+	//compute();
+        MPI_Wait(&req1, MPI_STATUS_IGNORE);
     } else if (rank == 1) {
 	MPI_Irecv(&x, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &req1);
+        MPI_Wait(&req1, MPI_STATUS_IGNORE);
     }
-    MPI_Wait(&req1, MPI_STATUS_IGNORE);
+    //MPI_Wait(&req1, MPI_STATUS_IGNORE);
     printf("rank %d: x=%d\n",rank,x);
 
     MPI_Finalize();
